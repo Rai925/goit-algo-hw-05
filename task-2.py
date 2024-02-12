8ae49a4ba615ec77933f1d82be32a31d06d00313
Rai925 <154880397+Rai925@users.noreply.github.com>
2024-02-12 19:38:38 +0200

    Update task-2.py

--- task-2.py
+++ task-2.py
@@ -1,16 +1,16 @@
+import re
 from typing import Callable
 from functools import reduce
-import re
 
 def generator_numbers(text: str):
-    numbers = map(float,filter(lambda x: re.match(r"\d+\.{0,1}\d+.",x),text.split(' ')))
+    numbers = map(float, re.findall(r"\s\b\d+(?:\.\d+)?\b\s", text))
     for number in numbers:
         yield number
 
 def sum_profit(text: str, func: Callable):
     return reduce(lambda x,y: x+y ,func(text))
-        
-if __name__=="__main__":
+
+if __name__ == "__main__":
     text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід,\
         доповнений додатковими надходженнями 27.45 і 324.00 доларів."
     total_income = sum_profit(text, generator_numbers)
